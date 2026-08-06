d9fb17d33ce020e57dc2921932a612414452b10b
Ernesto Cantu <ernesto.cantu@aleph5.com>
2026-08-06 09:14:47 -0600

    DDL

--- database/ddl.sql
+++ database/ddl.sql
@@ -2,7 +2,7 @@
 -- Tabla usuarios: Registra a los usuarios de MarIA, con el telegram_user_id. Solo accede desde Telegram.
 -- Tabla activities: Registra las actividades de los usuarios, con título, fecha de vencimiento, prioridad y estado. 
 -- Stored Procedure sp_cleanup_completed_tasks: Elimina las actividades completadas de un usuario específico.
-
+create database maria_pa;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -54,4 +54,25 @@ END$$
 
 DELIMITER ;
 
-CALL sp_cleanup_completed_tasks(1);
+--CALL sp_cleanup_completed_tasks(1);
+
+
+
+INSERT INTO maria_pa.users (telegram_user_id,name,`role`,status,created_at,updated_at) VALUES
+	 (5117008703,'Ernesto Cantu','ADMIN','ACTIVE','2026-07-31 16:57:22','2026-07-31 16:57:22'),
+	 (6554118716,'danonina','USER','ACTIVE','2026-08-04 21:27:04','2026-08-04 21:27:41');
+
+
+INSERT INTO maria_pa.activities (user_id,title,due_date,priority,status,created_at,updated_at) VALUES
+	 (1,'cita visa de las chicas','2026-08-18','URGENT','IN_PROGRESS','2026-08-02 14:35:08','2026-08-04 21:42:23'),
+	 (2,'Eres hermosa ❤️... att el admin','2026-08-05','URGENT','IN_PROGRESS','2026-08-02 20:51:53','2026-08-04 21:29:05'),
+	 (2,'Prueba actividad adicional','2026-08-05','MEDIUM','IN_PROGRESS','2026-08-02 20:51:53','2026-08-04 21:43:04'),
+	 (1,'Preparar demo SPD','2026-08-05','URGENT','IN_PROGRESS','2026-08-04 22:47:43','2026-08-04 22:56:55'),
+	 (1,'Avanzar con curso cyber','2026-08-05','LOW','IN_PROGRESS','2026-08-04 22:47:43','2026-08-04 22:56:55'),
+	 (1,'Continuar con SLR','2026-08-05','URGENT','IN_PROGRESS','2026-08-04 22:47:43','2026-08-04 22:56:55'),
+	 (1,'Activar el server Ubuntu','2026-08-05','MEDIUM','IN_PROGRESS','2026-08-04 22:50:37','2026-08-04 22:56:55'),
+	 (2,'Cumpleaños de Exa González','2026-10-22','MEDIUM','IN_PROGRESS','2026-08-05 10:31:32','2026-08-05 10:31:32'),
+	 (2,'Cumpleaños de Claudia Escamilla','2026-08-08','MEDIUM','IN_PROGRESS','2026-08-05 10:33:06','2026-08-05 10:33:06');
+
+
+
